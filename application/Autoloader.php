073676817207c6451e5332faf2f38bed8a4d4bb0
Your Name <bill@prgmrbill.com>
2011-11-09 06:46:19 -0500

    Fixing EOLs and commenting

--- application/Autoloader.php
+++ application/Autoloader.php
@@ -1,1 +1,46 @@
-<?php error_reporting(-1);ini_set('display_errors', 1);$root	   = str_replace('application', '', __DIR__);$paths 	   = array();// Models$paths[]   = sprintf('%smodel', $root);$paths[]   = sprintf('%sapplication', $root);$paths[]   = sprintf('%stest', $root);$paths[]   = get_include_path();function autoload($className){	switch( $className )	{		// Password hashing		case 'PasswordHash':			require sprintf('%s/application/thirdparty/phpass/PasswordHash.php', 							str_replace('application', '', __DIR__));			return true;				// OpenID 		case 'LightOpenID':			require sprintf('%s/application/thirdparty/lightopenid/openid.php', 							str_replace('application', '', __DIR__));			return true;					// Everything else		default:			require sprintf('%s.class.php', $className);			return true;	}}// Add your class dir to include pathset_include_path(implode(PATH_SEPARATOR, $paths));// You can use this trick to make autoloader look for commonly used "My.class.php" type filenamesspl_autoload_extensions('.class.php,.php');// Use default autoload implementationspl_autoload_register('autoload');
+<?php 
+/*
+ * Init and autoloading
+ *
+ */
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+$root	   = str_replace('application', '', __DIR__);
+$paths 	   = array();
+$paths[]   = sprintf('%smodel', $root);
+$paths[]   = sprintf('%sapplication', $root);
+$paths[]   = sprintf('%stest', $root);
+$paths[]   = get_include_path();
+
+function autoload($className)
+{
+	switch( $className )
+	{
+		// TODO: Add these paths to the above array to eliminate the need for this
+		// switch statement
+		
+		// Password hashing
+		case 'PasswordHash':
+			require sprintf('%s/application/thirdparty/phpass/PasswordHash.php', 
+							str_replace('application', '', __DIR__));
+			return true;
+		
+		// OpenID 
+		case 'LightOpenID':
+			require sprintf('%s/application/thirdparty/lightopenid/openid.php', 
+							str_replace('application', '', __DIR__));
+			return true;
+			
+		// Everything else
+		default:
+			require sprintf('%s.class.php', $className);
+			return true;
+	}
+}
+
+set_include_path(implode(PATH_SEPARATOR, $paths));
+spl_autoload_extensions('.class.php,.php');
+spl_autoload_register('autoload');
+
+
--- application/Database.class.php
+++ application/Database.class.php
@@ -1,1 +1,44 @@
-<?php/* * Simple database access * */class Database {	protected $exMsg = '';		public function __construct()	{		}	// @param string $dsn - dsn for connection	// @param string $username	// @param string $password	public function GetConnection($dsn, $user, $password)	{		try		{			$connection = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);			$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);			$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);			return $connection;		}		catch(PDOException $e)		{			$this->exMsg = $e->getMessage();			return false;		}	}		protected function AddExceptionMsg($msg)	{		$this->exMsg = $msg;	}		public function GetExceptionMsg()	{		return $this->exMsg;	}}
+<?php
+/* 
+ * Simple rudimentary database access
+ *
+ */
+class Database 
+{
+	protected $exMsg = '';
+	
+	public function __construct()
+	{
+	
+	}
+
+	// @param string $dsn - dsn for connection
+	// @param string $username
+	// @param string $password
+	public function GetConnection($dsn, $user, $password)
+	{
+		try
+		{
+			$connection = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+			$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+			return $connection;
+		}
+		catch(PDOException $e)
+		{
+			$this->exMsg = $e->getMessage();
+			return false;
+		}
+	}
+	
+	protected function AddExceptionMsg($msg)
+	{
+		$this->exMsg = $msg;
+	}
+	
+	public function GetExceptionMsg()
+	{
+		return $this->exMsg;
+	}
+}
